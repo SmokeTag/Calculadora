ec0e35cd1dc3e4bf578dee40b67d2a3b69b29b30
André Thomas Monteiro <andre.tmonteiro@hyundai-brasil.com>
2025-05-09 22:31:37 -0300

    Refactor NormalCalculatorWidget by removing unused imports and simplifying the layout setup

--- interface_normal.py
+++ interface_normal.py
@@ -1,26 +1,14 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QGridLayout, QLineEdit, QSizePolicy
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QPushButton, QWidget, QGridLayout, QLineEdit, QSizePolicy
 from PyQt5.QtCore import Qt
 
-class MainWindow(QMainWindow):
+class NormalCalculatorWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
         
     def init_ui(self):
-        self.setWindowTitle("Normal Mode")
-        self.setGeometry(0, 0, 320, 400)
-        screen = QApplication.primaryScreen().availableGeometry()
-        x = (screen.width() - self.width()) // 2
-        y = (screen.height() - self.height()) // 2
-        self.move(x, y)
-        self.setWindowIcon(QIcon("Calculator_icon.png"))
-        
-        # Set up the main layout
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-        self.layout = QGridLayout(self.central_widget)
+        # Set up the layout
+        self.layout = QGridLayout(self)
         
         self.display = QLineEdit()
         self.display.setReadOnly(True)
@@ -35,27 +23,24 @@ class MainWindow(QMainWindow):
         ("*", 3, 3, 1, 1), ("/", 2, 2, 1, 1), ("<-", 2, 3, 1, 1),
         ("Clear", 2, 0, 1, 2), ("=", 6, 2, 1, 1)
     ]
-        self.button_map = {}  # Store buttons for later connections
+        self.button_map = {}
         for text, row, col, rowspan, colspan in buttons:
             btn = QPushButton(text)
             self.layout.addWidget(btn, row, col, rowspan, colspan)
             self.button_map[text] = btn
-        
         self.button_map["+"].setObjectName("add")
         self.button_map["+"].setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
         
-        
+
         self.setStyleSheet("""
             QLineEdit {
-            font-size: 24px;
-            padding: 10px;
-            border: 1px solid #e4dcd3;
+                font-size: 24px;
+                padding: 10px;
+                border: 1px solid #e4dcd3;
             }
-                           
             QPushButton {
                 font-size: 20px;
                 font-family: 'Calibri';
-                
                 background-color: #f6f3f2;
                 border: 1px solid #e4dcd3;
                 padding: 10px 12px;
@@ -68,10 +53,5 @@ class MainWindow(QMainWindow):
                 background-color: #00aad2;
             }
         """)
-        
+
     
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
