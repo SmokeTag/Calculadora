a53a85125d78ac2100620472d4c20332c49e6a7c
André Thomas Monteiro <andre.tmonteiro@hyundai-brasil.com>
2025-05-13 14:42:32 -0300

    Add ModeSelector class for switching between Normal and Scientific calculator modes

---
+++ menu_selector.py
@@ -0,0 +1,29 @@
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton
+
+class ModeSelector(QWidget):
+    def __init__(self, stack_widget, normal_widget, scientific_widget):
+        super().__init__()
+        self.stack_widget = stack_widget
+        self.normal_widget = normal_widget
+        self.scientific_widget = scientific_widget
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QHBoxLayout(self)
+        self.normal_btn = QPushButton("Normal")
+        self.scientific_btn = QPushButton("Scientific")
+        layout.addWidget(self.normal_btn)
+        layout.addWidget(self.scientific_btn)
+        self.normal_btn.clicked.connect(self.show_normal)
+        self.scientific_btn.clicked.connect(self.show_scientific)
+        self.show_normal()
+
+    def show_normal(self):
+        self.stack_widget.setCurrentWidget(self.normal_widget)
+        self.normal_btn.setEnabled(False)
+        self.scientific_btn.setEnabled(True)
+
+    def show_scientific(self):
+        self.stack_widget.setCurrentWidget(self.scientific_widget)
+        self.normal_btn.setEnabled(True)
+        self.scientific_btn.setEnabled(False)
