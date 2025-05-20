b8c2fe392d698c1f7897fab3e0c48a7b99ce060f
André Thomas Monteiro <andre.tmonteiro@hyundai-brasil.com>
2025-05-19 22:24:33 -0300

    Refactor HistoryCalculatorWidget to use QTextEdit for display and simplify button functionality

--- interface_history.py
+++ interface_history.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QPushButton, QWidget, QVBoxLayout, QLineEdit, QSizePolicy
+from PyQt5.QtWidgets import QPushButton, QWidget, QVBoxLayout, QTextEdit, QSizePolicy
 from PyQt5.QtCore import Qt
 from calculator_logic import CalculatorLogic
 
@@ -8,34 +8,36 @@ class HistoryCalculatorWidget(QWidget):
         self.logic = logic
         self.button_map = {}
         self.history_in_display = []
-        self.history_in_display_index = 0
+        # self.history_in_display_index = 0
         self.init_ui()
         
     def init_ui(self):
         self.layout = QVBoxLayout(self)
-        self.display = QLineEdit()
+        self.display = QTextEdit()
         self.display.setReadOnly(True)
         self.display.setAlignment(Qt.AlignRight)
+        self.display.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
         
-        buttons = ["Update History", "Clear History", "↑", "↓"]
+        # buttons = ["Update History", "Clear History", "↑", "↓"]
+        buttons = ["Clear History"]
         for text in buttons:
             btn = QPushButton(text)
             self.button_map[text] = btn
 
-        self.layout.addWidget(self.button_map["Update History"])
+        # self.layout.addWidget(self.button_map["Update History"])
         self.layout.addWidget(self.button_map["Clear History"])
-        self.layout.addWidget(self.button_map["↑"])
+        # self.layout.addWidget(self.button_map["↑"])
         self.layout.addWidget(self.display)
-        self.layout.addWidget(self.button_map["↓"])
+        # self.layout.addWidget(self.button_map["↓"])
         
-        self.button_map["Update History"].clicked.connect(self.update_history)
+        # self.button_map["Update History"].clicked.connect(self.update_history)
         self.button_map["Clear History"].clicked.connect(self.clear_history)
-        self.button_map["↑"].clicked.connect(self.scroll_to_previous_expressions)
-        self.button_map["↓"].clicked.connect(self.scroll_to_next_expressions)
+        # self.button_map["↑"].clicked.connect(self.scroll_to_previous_expressions)
+        # self.button_map["↓"].clicked.connect(self.scroll_to_next_expressions)
 
     def update_history(self):
-        self.history_in_display = self.logic.history[-6:]
-        self.history_in_display_index = 0
+        self.history_in_display = self.logic.history[::-1]
+        # self.history_in_display_index = 0
 
         if self.history_in_display:
             self.display.setText("\n\n".join(f"{expr} \n= {result}" for expr, result in self.history_in_display))
@@ -47,20 +49,19 @@ class HistoryCalculatorWidget(QWidget):
         self.history_in_display.clear()
         self.display.clear()
 
-    def scroll_to_previous_expressions(self):
-        if self.history_in_display_index > -len(self.logic.history) + 1:
-            self.history_in_display_index -= 1
-            start_index = self.history_in_display_index - 6
-            self.history_in_display = self.logic.history[start_index:self.history_in_display_index]
-            self.display.setText("\n\n".join(f"{expr} \n= {result}" for expr, result in self.history_in_display))
-        print(self.history_in_display_index)
-        print(self.history_in_display)
+    # def scroll_to_previous_expressions(self):
+    #     if self.history_in_display_index > -len(self.logic.history) + 1:
+    #         self.history_in_display_index -= 1
+    #         start_index = self.history_in_display_index - 6
+    #         self.history_in_display = self.logic.history[start_index:self.history_in_display_index]
+    #         self.display.setText("\n\n".join(f"{expr} \n= {result}" for expr, result in self.history_in_display))
 
-    def scroll_to_next_expressions(self):
-        if self.history_in_display_index < 0:
-            self.history_in_display_index += 1
-            self.history_in_display = self.logic.history[self.history_in_display_index-6:self.history_in_display_index]
-            self.display.setText("\n\n".join(f"{expr} \n= {result}" for expr, result in self.history_in_display))
-        print(self.history_in_display_index)
-        print(self.history_in_display)
+    # def scroll_to_next_expressions(self):
+    #     if self.history_in_display_index < 0:
+    #         self.history_in_display_index += 1
+    #         if self.history_in_display_index == 0:
+    #             self.update_history()
+    #             return
+    #         self.history_in_display = self.logic.history[self.history_in_display_index-6:self.history_in_display_index]
+    #         self.display.setText("\n\n".join(f"{expr} \n= {result}" for expr, result in self.history_in_display))
 
